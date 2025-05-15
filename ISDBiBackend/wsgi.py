28b31f4a0b7971589d14bc5b7705b01c1576aa6f
Abdelhak Chellal <abdelhakchellal@Abdelhaks-MacBook-Pro.local>
2025-05-15 18:02:55 +0100

    initial project

---
+++ ISDBiBackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ISDBiBackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ISDBiBackend.settings')
+
+application = get_wsgi_application()
---
+++ ReverseTransactions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReversetransactionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ReverseTransactions'
